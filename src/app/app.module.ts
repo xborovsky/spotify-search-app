c3fcbef04a2fb13eae37ed4efdfb21dc6c208637
Marek Borovsky <borovsky@oksystem.cz>
2017-08-30 10:18:44 +0200

    Routes, bootswatch, search component (GUI)

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,17 +2,23 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
+import { RouterModule } from '@angular/router';
+
+import { routes } from './app.routes';
 
 import { AppComponent } from './app.component';
+import { SearchComponent } from './search/search.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SearchComponent
   ],
   imports: [
     BrowserModule,
     FormsModule,
-    HttpModule
+    HttpModule,
+    RouterModule.forRoot(routes)
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/app.routes.ts
@@ -0,0 +1,8 @@
+import { Routes } from '@angular/router';
+
+import { SearchComponent } from './search/search.component';
+
+export const routes:Routes =[
+    { path: '', redirectTo: 'search', pathMatch : 'full' },
+    { path: 'search',  component : SearchComponent}
+];
